a8973eb2f408fff5569157d72aba7905b670fde9
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2022-08-08 17:20:43 -0300

    Delete pet directory

--- pet/conecta.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost" , "root" , "" , "pet");//"ip , user, password , data base name"
-    if(!$con){
-        echo mysqli_error();
-    }
-
-
-
-?>
--- pet/menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include "conecta.php";
-    $usuario = $_SESSION["user"];
-    $menu_query = "SELECT * FROM usuario WHERE login = '$usuario' AND tipo = '1'";
-    
-?>
