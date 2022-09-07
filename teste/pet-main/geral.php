a803165e48a45d27caebab1b45c6d842a4a144ba
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2022-09-07 17:20:11 -0300

    Add files via upload

---
+++ teste/pet-main/geral.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visão Geral DogCat</title>
+    <link rel="stylesheet" href="style.css">    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway:200,400,600" rel="stylesheet">
+
+</head>
+<body>
+    <!-- cabeÃ§alho -->
+    <main> 
+        <header>
+            <div class="mascara">
+                <h1> <strong>Pet Shop</strong> DogCat</h1>
+                <img class="seta" src="images/seta.svg" alt="">
+            </div>
+        </header>
+    </main>
+    
+    <!-- PortifÃ³lio-->
+    <section class="portifolio">
+        <div class="container">
+            <h1>Nossa Empresa</h1>
+            <ul>
+                <li><a>Desde 2000!</a></li>
+                <li><a>Somos uma empresa disposta a realizar o melhor serviço para seu pet!</a></li>
+                <li><a>Linha completa de rações e suplementos para seu pet, além de serviços de beleza, creche e muito mais!</a></li>
+                <li><a>Nosso endereço: R.Escorrega Lá Vai Um, 3632 CEP 40028-922</a></li>
+            </ul>
+        </div>
+    </section>
+    
+    <!-- Sobre -->
+    <section class="sobre separador-esquerda">
+        <div class="container">
+            <h1>Entre em contato</h1>
+            <p>Telefone: 4002-8922</p>
+            <p>Email: petdogcat@yopmail.com</p>
+        </div>
+    </section>
+
+</body>
+</html>
